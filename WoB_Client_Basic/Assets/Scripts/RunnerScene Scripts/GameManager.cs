2ea1d47f30c48098172e0a0c7ba04580d993b8e9
ZTGeng <zhtgeng@gmail.com>
2015-04-13 15:35:20 -0700

    client basic
    1. add GameLogic.Running
    2. initialize player1, player2, items
    3. empty HeartBeat

--- WoB_Client_Basic/Assets/Scripts/RunnerScene Scripts/GameManager.cs
+++ WoB_Client_Basic/Assets/Scripts/RunnerScene Scripts/GameManager.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class GameManager : MonoBehaviour {
 
-	public GameObject player;
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject item1;
+	public ArrayList items;
 	private GameCamera cam;
     private float raceTime;
 
@@ -12,18 +16,32 @@ public class GameManager : MonoBehaviour {
 
 		cam = GetComponent<GameCamera> ();
 		SpawnPlayer ();
+		Instantiate (player2, Vector3.zero, Quaternion.identity);
         raceTime = 0;
+		//Debug.Log("Before!!!!!!!!!");
+
+		items = new ArrayList();
+		//repeat until none
+		// item = new GO();
+		// PlaItem(item, xy);
+		// items.Add (items);
+
+		GameObject.Find("GameLogic").GetComponent<Running>().RunOnce();
 
 	}
 	
 	private void SpawnPlayer() {
-		cam.SetTarget(( Instantiate (player, Vector3.zero, Quaternion.identity) as GameObject).transform);
+		cam.SetTarget(( Instantiate (player1, Vector3.zero, Quaternion.identity) as GameObject).transform);
+	}
+
+	private void PlaceItem(GameObject itemType, Vector2 vect) {
+		Instantiate (itemType, Vector3.zero, Quaternion.identity);
 	}
 
     void update()
     {
         Debug.Log("this gets called");
-        raceTime += Time.deltaTime;
-        Debug.Log(raceTime);
+ //       raceTime += Time.deltaTime;
+ //       Debug.Log(raceTime);
     }
 }
---
+++ WoB_Client_Basic/Assets/Scripts/RunnerScene Scripts/Running.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Running : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+	private bool flag = true;
+
+	public void RunOnce () {
+
+		//Debug.Log("After!!!!!!!!");
+
+		player1 = GameObject.Find("Player_sprite(Clone)");
+		player2 = GameObject.Find("Player_sprite_2(Clone)");
+//		if (player1 != null) {
+//			Debug.Log("found " + player1.transform.position);
+//		}
+
+	}
+
+	private void Player2Move(Vector2 newVect) {
+		player2.transform.position = newVect;
+	}
+
+	private void HeartBeat() {
+		// The Heart Beat!!
+	}
+
+	private IEnumerator Delay() {
+		//
+		//Debug.Log("Before!!!!!!!!!");
+		flag = false;
+		yield return new WaitForSeconds(0.1f);
+		//Debug.Log("inside!!!!!!!!!");
+		//Player2Move( new Vector2(player1.transform.position.x, player1.transform.position.y + 3) );
+		HeartBeat();
+		flag = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (flag) {
+			StartCoroutine(Delay());
+		}
+
+		//Debug.Log("this gets called");
+		//Debug.Log("outside!!!!!!!!!");
+
+	}
+}
