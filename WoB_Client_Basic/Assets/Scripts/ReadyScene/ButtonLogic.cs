e9fcec025737d33926fb0ff05e8f8f29c1e5b272
Dong2014 <dongli2468@yahoo.com>
2015-04-13 20:30:10 -0700

    try to make connection to the server

--- WoB_Client_Basic/Assets/Scripts/ReadyScene/ButtonLogic.cs
+++ WoB_Client_Basic/Assets/Scripts/ReadyScene/ButtonLogic.cs
@@ -3,18 +3,29 @@ using System.Collections;
 
 public class ButtonLogic : MonoBehaviour {
 
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+		
+		gameObject.AddComponent("MessageQueue");
+		gameObject.AddComponent("ConnectionManager");
+
+	}
+	
 	// Use this for initialization
 	void Start () {
-	
+
+		ConnectionManager cManager = gameObject.GetComponent<ConnectionManager>();
+		
+		if (cManager) {
+			cManager.setupSocket();
+
+		}
 	}
 
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	public void Ready(){
 		//for now just go to the game scene
 		Application.LoadLevel ("RunnerScene");
 	}
+
 }
