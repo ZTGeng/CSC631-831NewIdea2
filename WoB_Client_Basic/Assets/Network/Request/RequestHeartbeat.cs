18d5473877ca671e9793c019855ab32fe09a0a3a
Dong2014 <dongli2468@yahoo.com>
2015-04-16 15:37:36 -0700

    add reqeust and respond

--- WoB_Client_Basic/Assets/Network/Request/RequestHeartbeat.cs
+++ WoB_Client_Basic/Assets/Network/Request/RequestHeartbeat.cs
@@ -8,12 +8,13 @@ public class RequestHeartbeat : NetworkRequest {
 		request_id = Constants.CMSG_HEARTBEAT;
 	}
 	
-	public void send(float x,float y,float playerDistanceTraveled) {
+	public void send(float x,float y,float playerDistanceTraveled,short gameover) {
 	
 	    packet = new GamePacket(request_id);
 		packet.addFloat32 (x);
 		packet.addFloat32 (y);
 		packet.addFloat32 (playerDistanceTraveled);
+		packet.addShort16 (gameover);
 
 	}
 }
--- WoB_Client_Basic/Assets/Network/Response/ResponseInGameHeartbeat.cs
+++ WoB_Client_Basic/Assets/Network/Response/ResponseInGameHeartbeat.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ResponseInGameHeartbeat : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+public class ResponseInGameHeartbeatEventArgs : ExtendedEventArgs {
+	public float opponentX { get; set; }
+	public float opponenty { get; set; }
+	public float opponentDistanceTraveled  { get; set; }
+
+
+	public ResponseInGameHeartbeatEventArgs() {
+		event_id = Constants.SMSG_IN_GAME_HEARTBEAT;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+}
+
+public class ResponseInGameHeartbeat : NetworkResponse {
+	private float opponentX,opponentY,opponentDistanceTraveled;
+
+	public ResponseInGameHeartbeat() {
+
 	}
+	public override void parse() {
+
+		opponentX = DataReader.ReadFloat (dataStream);
+		opponentY = DataReader.ReadFloat (dataStream);
+		opponentDistanceTraveled = DataReader.ReadFloat (dataStream);
+
+
+	}
+	public override ExtendedEventArgs process() {
+
+		ResponseInGameHeartbeatEventArgs args = new ResponseInGameHeartbeatEventArgs ();
+		args.opponentX = opponentX;
+		args.opponenty = opponentY;
+		args.opponentDistanceTraveled = opponentDistanceTraveled;
+		return args;
+
+
+
+	}
+
+
+
 }
