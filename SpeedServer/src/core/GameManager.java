887895a91dc28ebdd6c21a366ceac1977dd1be9f
eltortuga <mds831@gmail.com>
2015-04-19 23:42:06 -0700

    Trying to set up the game server

--- SpeedServer/src/core/GameManager.java
+++ SpeedServer/src/core/GameManager.java
@@ -4,11 +4,57 @@
  * and open the template in the editor.
  */
 package core;
+import model.Game;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author markfavis
  */
 public class GameManager {
+    
+    protected List<Game> games;
+//    protected HashMap 
+    protected short numberOfGamesBeingPlayed;
+    
+    private final short MAX_NUMBER_OF_PLAYERS = 2;
+    
+    GameManager(){
+        games = new ArrayList<Game>();
+        numberOfGamesBeingPlayed = 0;
+        
+    }
+    
+    //Adds a client to the correct game out of a list of games and makes sure there is a game for the client to be added to.
+    public void addClientToGame(GameClient client) {
+        noCurrentGames();
+        PlaceClient(client);
+        createNewGame();
+    }
+    
+    //checks to see if any games exisit and if not creates one;
+    void noCurrentGames() {
+        if (games.isEmpty())
+            games.add(new Game(null, null));
+    }
+    
+    //Places the client into the open game with the correct player position. index of the last index in the list of games which will always be a non full game;
+    void PlaceClient(GameClient client) {        
+        switch (games.get(games.size()).getNumberOfPlayersInGame()){
+            case 0: games.get(games.size()).setPlayer1(client);
+                    break;
+            case 1: games.get(games.size()).setPlayer2(client);
+                    break;
+        }
+    }    
+    
+    // Checks to see if the last game in the list is full and if so creates a new one.
+    void createNewGame() {
+        if (games.get(games.size()).getNumberOfPlayersInGame() == MAX_NUMBER_OF_PLAYERS){
+            games.add(new Game(null, null));
+        }
+    }
     
 }
--- SpeedServer/src/model/Game.java
+++ SpeedServer/src/model/Game.java
@@ -1,4 +1,5 @@
 package model;
+import core.GameClient;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,61 +13,128 @@ package model;
  */
 public class Game {
     
-    private int uniqueGameInstanceID;
-    private String player1SessionID;
-    private String player2SessionID;
+//    private 
+    private short shortPlayersInGame;
+    private GameClient player1;
+    private GameClient player2;
     
     public Game(){
-        
+        shortPlayersInGame = 0;
+    }
+   
+    public Game(GameClient player1, GameClient player2){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    /**
+     * @return the player1
+     */
+    public GameClient getPlayer1() {
+        return player1;
+    }
+
+    /**
+     * @return the player2
+     */
+    public GameClient getPlayer2() {
+        return player2;
+    }
+
+    /**
+     * @param player1 the player1 to set
+     */
+    public void setPlayer1(GameClient player1) {
+        this.player1 = player1;
+        shortPlayersInGame++;
+    }
+
+    /**
+     * @param player2 the player2 to set
+     */
+    public void setPlayer2(GameClient player2) {
+        this.player2 = player2;
+        shortPlayersInGame++;
     }
     
-    public Game(int gameID, String player1, String player2){
-        this.uniqueGameInstanceID = gameID;
-        player1SessionID = player1;
-        player2SessionID = player2;
-    }
-
-    /**
-     * @return the uniqueGameInstanceID
-     */
-    public int getUniqueGameInstanceID() {
-        return uniqueGameInstanceID;
-    }
-
-    /**
-     * @return the player1SessionID
-     */
-    public String getPlayer1SessionID() {
-        return player1SessionID;
-    }
-
-    /**
-     * @return the player2SessionID
-     */
-    public String getPlayer2SessionID() {
-        return player2SessionID;
-    }
-
-    /**
-     * @param uniqueGameInstanceID the uniqueGameInstanceID to set
-     */
-    public void setUniqueGameInstanceID(int uniqueGameInstanceID) {
-        this.uniqueGameInstanceID = uniqueGameInstanceID;
-    }
-
-    /**
-     * @param player1SessionID the player1SessionID to set
-     */
-    public void setPlayer1SessionID(String player1SessionID) {
-        this.player1SessionID = player1SessionID;
-    }
-
-    /**
-     * @param player2SessionID the player2SessionID to set
-     */
-    public void setPlayer2SessionID(String player2SessionID) {
-        this.player2SessionID = player2SessionID;
+    public short getNumberOfPlayersInGame() {
+        return shortPlayersInGame;
     }
     
     
 }
+//
+//package model;
+//
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+///**
+// *
+// * @author markfavis
+// */
+//public class Game {
+//    
+//    private int uniqueGameInstanceID;
+//    private String player1;
+//    private String player2;
+//    
+//    public Game(){
+//        
+//    }
+//    
+//    public Game(int gameID, String player1, String player2){
+//        this.uniqueGameInstanceID = gameID;
+//        player1 = player1;
+//        player2 = player2;
+//    }
+//
+//    /**
+//     * @return the uniqueGameInstanceID
+//     */
+//    public int getUniqueGameInstanceID() {
+//        return uniqueGameInstanceID;
+//    }
+//
+//    /**
+//     * @return the player1
+//     */
+//    public String getPlayer1SessionID() {
+//        return player1;
+//    }
+//
+//    /**
+//     * @return the player2
+//     */
+//    public String getPlayer2SessionID() {
+//        return player2;
+//    }
+//
+//    /**
+//     * @param uniqueGameInstanceID the uniqueGameInstanceID to set
+//     */
+//    public void setUniqueGameInstanceID(int uniqueGameInstanceID) {
+//        this.uniqueGameInstanceID = uniqueGameInstanceID;
+//    }
+//
+//    /**
+//     * @param player1 the player1 to set
+//     */
+//    public void setPlayer1SessionID(String player1) {
+//        this.player1 = player1;
+//    }
+//
+//    /**
+//     * @param player2 the player2 to set
+//     */
+//    public void setPlayer2SessionID(String player2) {
+//        this.player2 = player2;
+//    }
+//    
+//    
+//}
+
+
