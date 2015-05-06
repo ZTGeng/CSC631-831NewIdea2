823d8fbbc575c547e2c29b89de6dc0e1eedf9c6c
eltortuga <mds831@gmail.com>
2015-05-05 18:42:37 -0700

    The games start at the same time now.

---
+++ WoB_Client_Basic/Assets/Network/Request/RequestRRStartGame.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+
+public class RequestRRStartGame : NetworkRequest {
+	
+	public RequestRRStartGame() {
+		packet = new GamePacket(request_id = Constants.CMSG_RRSTARTGAME);
+	}
+	
+	public void Send(int playerid) {
+		packet.addInt32(playerid);
+	}
+}
