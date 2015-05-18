f97d8347693f891a2c7a18b68b2ace3ac7392d01
Dong2014 <dongli2468@yahoo.com>
2015-05-18 13:57:59 -0700

    end game will tell who is winner or looser

--- WoB_Client_Basic/Assets/Scripts/EndScene/EndGameManager.cs
+++ WoB_Client_Basic/Assets/Scripts/EndScene/EndGameManager.cs
@@ -9,10 +9,24 @@ namespace RR {
 		public Canvas canvas;
 		public EventSystem sys;
 		public bool called = true;
+		private string text = " ";
+		private bool show = false;
 	
 		private GameObject obj;
 		// Use this for initialization
 		void Start () {
+			
+		}
+
+		void OnGUI(){
+			GUIStyle myStyle = new GUIStyle();
+			myStyle.normal.textColor = Color.blue;
+			myStyle.fontSize = 50;
+		
+			if(show)
+			{
+			GUI.Label (new Rect (Screen.width - 350, 200, 200, 100), text, myStyle);
+			}
 			
 		}
 		
@@ -21,10 +35,14 @@ namespace RR {
 			if (PlayerPrefs.GetInt ("Win") >= 0 && called) {
 				Debug.Log ("Winning Time is: " + PlayerPrefs.GetString ("Winning Time"));
 				if (PlayerPrefs.GetInt ("Win") == 1) {
+					show = true;
+					text = "YOU WIN!";
 					Debug.Log ("YOU WIN!");
 				} else {
 					Debug.Log ("YOU LOSE!");
+					show = true;
 					called = false;
+					text = "YOU LOSE";
 				}
 			}
 		}
